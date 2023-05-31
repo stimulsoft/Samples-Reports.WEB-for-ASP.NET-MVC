ff9c71ec3d630f8b07df52cc6fdeaac04786991d
Uladzimir Khotchanka <hotchenko@gmail.com>
2023-05-31 11:07:12 +0200

    fix framework version

--- Sending a Report by Email/Scripts/_references.js
+++ Sending a Report by Email/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-3.4.1.js" />
-/// <reference path="modernizr-2.8.3.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-3.4.1.js" />
 /// <autosync enabled="true" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
