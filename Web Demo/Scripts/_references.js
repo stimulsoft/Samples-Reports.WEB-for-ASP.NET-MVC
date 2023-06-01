878a85d1df8a705fe16142c1c268476d7c66f110
Uladzimir Khotchanka <hotchenko@gmail.com>
2023-06-01 08:08:56 +0200

    update jquery version

--- Web Demo/Scripts/_references.js
+++ Web Demo/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-3.4.1.js" />
+﻿/// <reference path="jquery-3.5.1.js" />
+/// <reference path="modernizr-2.8.3.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="dtree.js" />
